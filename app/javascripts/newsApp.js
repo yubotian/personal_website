b3616ef54a024b05b0c33aa6d4fec1122a8e29f4
Yubo Tian <yubo.d.tian@gmail.com>
2015-04-16 17:55:12 -0400

    main news templated created. model view connected

---
+++ app/javascripts/newsApp.js
@@ -0,0 +1,17 @@
+//chirpApp.js
+
+var app = angular.module( 'newsApp' , [] );
+
+app.controller ( 'mainController' , function( $scope ) {
+	$scope.posts = [] ;
+	$scope.newPost = { created_by : '' , text : '' , craeted_at : '' } ; //TODO: picture
+
+	$scope.post = function() {
+		$scope.newPost.created_at = Date.now() ;
+		$scope.posts.push ( $scope.newPost ) ;
+		$scope.newPost = { created_by : '' , text : '' , craeted_at : '' } ;
+	}
+
+	
+});
+
