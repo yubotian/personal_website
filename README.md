19e40a9a6605d8336945ae56ca73d404a6199317
Yubo Tian <yubo.d.tian@gmail.com>
2015-04-18 16:13:06 -0400

    more styling with glyphicon and added location

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # personal_website
 personal website using MEAN stack
+
+
+TODO:
+
+follow MVA to implement login/reg
+
+modify to hide post area unless logged in
+
+hide reg button, change login to admin
+
+populate other links (aboutme/resume)
+
+
+minor:
+created_by -> created location
--- app/javascripts/newsApp.js
+++ app/javascripts/newsApp.js
@@ -4,12 +4,12 @@ var app = angular.module( 'newsApp' , [] );
 
 app.controller ( 'newsController' , function( $scope ) {
 	$scope.posts = [] ;
-	$scope.newPost = { created_by : '' , text : '' , craeted_at : '' } ; //TODO: picture
+	$scope.newPost = { created_by : '' , created_location : '' , text : '' , created_time : '' } ; //TODO: picture
 
 	$scope.post = function() {
-		$scope.newPost.created_at = Date.now() ;
+		$scope.newPost.created_time = Date.now() ;
 		$scope.posts.push ( $scope.newPost ) ;
-		$scope.newPost = { created_by : '' , text : '' , craeted_at : '' } ;
+		$scope.newPost = { created_by: '' , created_location : '' , text : '' , created_time : '' } ;
 
 	}
 
