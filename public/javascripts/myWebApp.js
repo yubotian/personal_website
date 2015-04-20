48b628ddfe7e8c9d0e195e6d26fe70cabbae5772
Yubo Tian <yubo.d.tian@gmail.com>
2015-04-20 02:06:31 -0400

    added ngResource. entire app running locally

--- public/javascripts/myWebApp.js
+++ public/javascripts/myWebApp.js
@@ -1,12 +1,20 @@
-//chirpApp.js
+var app = angular.module( 'myWebApp' , ['ngRoute', 'ngResource'] ).run(function($rootScope) {
+  $rootScope.authenticated = false;
+  $rootScope.current_user = '';
+  
+  $rootScope.signout = function(){
+      $rootScope.authenticated = false;
+      $rootScope.current_user = '';
+      $http.get('auth/signout');
+  };
+});
 
-var app = angular.module( 'myWebApp' , ['ngRoute'] );
 
 app.config(function($routeProvider){
   $routeProvider
     //the timeline display
     .when('/', {
-      templateUrl: '../views/main.html',
+      templateUrl: './views/main.html',
       controller: 'newsController'
     })
     //the login display
@@ -15,8 +23,8 @@ app.config(function($routeProvider){
       controller: 'authController'
     })
     //the signup display
-    .when('/register', {
-      templateUrl: '../views/register.html',
+    .when('/signup', {
+      templateUrl: './views/register.html',
       controller: 'authController'
     })
     .when('/resume', {
@@ -26,34 +34,59 @@ app.config(function($routeProvider){
 });
 
 
+app.factory('postService', function($resource){
+  return $resource('/api/posts/:id');
+});
 
-app.controller ( 'newsController' , function( $scope ) {
-	$scope.posts = [] ;
+app.controller ( 'newsController' , function( $scope, postService ) {
+	$scope.posts = postService.query();
 	$scope.newPost = { created_by : '' , created_location : '' , text : '' , created_time : '' } ; //TODO: picture
 
+
 	$scope.post = function() {
-		$scope.newPost.created_time = Date.now() ;
-		$scope.posts.push ( $scope.newPost ) ;
-		$scope.newPost = { created_by: '' , created_location : '' , text : '' , created_time : '' } ;
+    $scope.newPost.created_by = "Yubo";
+    //alternatively: 
+    //$scope.newPost.created_by = $rootScope.current_user;
+    $scope.newPost.created_time = Date.now(); 
 
-	}
-
-	
+    postService.save($scope.newPost, function(){
+      $scope.posts = postService.query();
+      $scope.newPost = { created_by: '' , created_location : '' , text : '' , created_time : '' } ;
+    });
+    
+	};
 });
 
 
-app.controller('authController', function($scope){
+app.controller('authController', function($scope , $rootScope, $http , $location){
   $scope.user = {username: '', password: ''};
   $scope.error_message = '';
 
   $scope.login = function(){
-    //placeholder until authentication is implemented
-    $scope.error_message = 'login request for ' + $scope.user.username;
+    $http.post('/auth/login', $scope.user).success(function(data){
+      if(data.state == 'success'){
+        $rootScope.authenticated = true;
+        $rootScope.current_user = data.user.username;
+        //redirect user
+        $location.path('/');
+      }
+      else{
+        $scope.error_message = data.message;
+      }
+    });
   };
 
   $scope.register = function(){
-    //placeholder until authentication is implemented
-    $scope.error_message = 'registration request for ' + $scope.user.username;
+    $http.post('/auth/signup', $scope.user).success(function(data){
+      if(data.state == 'success'){
+        $rootScope.authenticated = true;
+        $rootScope.current_user = data.user.username;
+        $location.path('/');
+      }
+      else{
+        $scope.error_message = data.message;
+      }
+    });
   };
 });
 
